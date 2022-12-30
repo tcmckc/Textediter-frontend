f350f03a8db27a5dde4a0854595bef7a4584ecfa
Tomoko Ishii <tomoko.s.ishii@gmail.com>
2022-12-30 09:10:07 +0100

    added socket

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+//import { io } from "socket.io-client";
 
 import Editor from './components/Editor';
+//import { useEffect, useState } from 'react';
+
+//let socket;
 
 
 function App() {
+  // const [socket, setSocket] = useState(null);
+
+  // useEffect(() => {
+  //   setSocket(io("http://localhost:8976"));
+
+  //   return () => {
+  //     if (socket) {
+  //       socket.disconnect();
+  //     }
+  //   }
+  // }, [socket]); 
+
+
+  // socket.emit('create', docs['_id']);
+  // socket.to(data['_id']).emit('doc', data);
+
   return (
     <div className="App">
       <header className="header">
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 import './../App.css';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { io } from "socket.io-client";
 
 import List from './List';
 import Save from './Save';
 import Name from './Name';
+import { useEffect } from 'react';
+
+const socket = io('http://localhost:1337');
 
 function Editor () {
   const [id, setId] = useState();
   const [name, setName] = useState();
   const [text, setText] = useState();
   const [newname, setNewname] = useState();
+  const [newtext, setNewtext] = useState();
 
   function setInfo (doc) {
     setId(doc[0]._id);
@@ -19,14 +24,28 @@ function Editor () {
     setText(doc[0].text);
   };
 
-    return (
-        <div>
-            <List onSelect={setInfo} />
-            <Name newname={newname} onChange={setNewname} />
-            <ReactQuill theme="snow" value={text} onChange={setText}/>
-            <Save value={text} id={id} name={name} newname={newname}/>
-        </div>
-    )
+  useEffect(() => {
+    // (async () => {
+    //   await setNewtext(text);
+    // })();
+
+    // Send to server
+    socket.emit('send_text', { message: text });
+  },[text]);
+
+  //  Receive from server
+  socket.on('received_text', (data) => {
+    setText(data.message);
+  });
+
+  return (
+      <div>
+          <List onSelect={setInfo} />
+          <Name newname={newname} onChange={setNewname} />
+          <ReactQuill theme="snow" value={text} onChange={setText} />
+          <Save value={text} id={id} name={name} newname={newname}/>
+      </div>
+  );
 }
 
 export default Editor;
